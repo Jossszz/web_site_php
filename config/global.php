966c958348cef472bc745d475faab4945bb1b664
jossszz <josejossszz@gmail.com>
2024-08-31 22:15:49 -0400

    Se agrega el Proyecto

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php
+//Ip del servidor de la bd
+define("DB_HOST", "localhost");
+
+//Nombre de la bd
+define("DB_NAME", "dbwebphp");
+
+//Usuario de la bd
+define("DB_USERNAME", "root");
+
+//Contraseña del usuario de la bd
+define("DB_PASSWORD", "");
+
+//Definir la codificacion de los caracteres
+define("DB_ENCODE", "UTF8");
+
+//Definir la constante como nombre del proyecto
+define("PRO_NOMBRE", "ITVentas");
+?>
